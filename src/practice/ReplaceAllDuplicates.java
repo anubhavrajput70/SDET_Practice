246c7ea2c2609010082f5aaabdfd29e96a20f20c
anubhavrajput193@gmail.com <anubhavrajput193@gmail.com>
2025-03-31 03:15:26 +0530

    Initial commit

---
+++ src/practice/ReplaceAllDuplicates.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReplaceAllDuplicates {
+
+	public static void main(String[] args) {
+		String s = "Automation";
+		System.out.println(replaceDuplicates(s.toLowerCase()));
+	}
+
+	public static String replaceDuplicates(String s) {
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for (int i = 0; i < s.length(); i++) {
+			map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+		}
+		StringBuilder result = new StringBuilder();
+		for (char ch : s.toCharArray()) {
+			int count = map.get(ch);
+			if (count > 1) {
+				result.append(count);
+			} else {
+				result.append(ch);
+			}
+		}
+		return result.toString();
+	}
+
+}
